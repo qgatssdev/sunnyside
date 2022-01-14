17bf09f41e8abe16fb51726883fdd743d83dd8ba
qgatssdev <chivoomodu@gmail.com>
2022-01-14 15:19:06 +0100

    jfk

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ function App() {
         alt="transform"
         className="transform"
       />
+      <div className='transform-text'>
+        <h2>Transform your brand</h2>
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you
+        </p>
+      </div>
     </div>
   );
 }
