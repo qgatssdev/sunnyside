cb1531140109014fbefcdb5d93ba2c43a8e8f72c
qgatssdev <chivoomodu@gmail.com>
2022-01-14 13:33:38 +0100

    still working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        
-      </header>
-
+      <div className="Header">
+        <h1>Sunny Side</h1>
+        <p>We are Creatives</p>
+      </div>
+      <img
+        src={require('./images/mobile/image-transform.jpg')}
+        alt="transform"
+        className="transform"
+      />
     </div>
   );
 }
